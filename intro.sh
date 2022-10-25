d1aafc7d534aa32d065f88409210d8a19ab5b47b
Jade <112060601+poisk-ls@users.noreply.github.com>
2022-10-25 10:45:42 +0800

    Update intro.sh

--- intro.sh
+++ intro.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/bash
+
 echo "Enter name"
+
 read name
-#copies the following code to motd.sh
-echo "echo "★★★★★★★★★★★★★★★★★★★★★★★★★★★★"
-echo "Welcome to Termux $name! 😈"
-echo "★★★★★★★★★★★★★★★★★★★★★★★★★★★★"
-figlet $name
+#Copy the following code to motd.sh
+
+echo "echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶"
+echo "Welcome to Termux ☣️$name! ☣️"
+echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶"
+figlet $name | lolcat
 neofetch" > motd.sh
 #tries to remove motd file if any
 rm /data/data/com.termux/files/usr/etc/motd
@@ -14,4 +17,6 @@ rm /data/data/com.termux/files/usr/etc/motd.sh
 #moves the created motd.sh file to profile.d directory
 mv motd.sh /data/data/com.termux/files/usr/etc/profile.d 
 clear
+
 echo "Please restart Termux now to confirm changes. Enjoy!"
+⠀⠀

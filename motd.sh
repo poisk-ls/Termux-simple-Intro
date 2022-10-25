155e1f211fe732575635b0f0690716752ad79f05
Jade <112060601+poisk-ls@users.noreply.github.com>
2022-10-25 11:08:19 +0800

    Add files via upload

---
+++ motd.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+echo "Enter name"
+read name
+#Copy the following code to motd.sh
+echo "echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶"
+echo "echo "        Welcome to Termux $name! ☣️"
+echo "echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶"
+figlet $name | lolcat
+neofetch" > motd.sh | lolcat
+#Tries to remove motd file if any.
+rm /data/data/com.termux/files/usr/etc/motd
+#Tries to remove motd.sh file if any
+rm /data/data/com.termux/files/usr/etc/motd.sh
+#Move the created motd.sh file to profile.d directory
+mv motd.sh /data/data/com.termux/files/usr/etc/profile.d
+clear
+
+echo "Please restart Termux now to confirm changes. Enjoy!" | lolcat
+

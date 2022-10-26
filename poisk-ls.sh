4ec5c1ca4b314af875d01cd7710b23e9eed4545d
Jade <112060601+poisk-ls@users.noreply.github.com>
2022-10-26 17:48:42 +0800

    Update poisk-ls.sh

--- poisk-ls.sh
+++ poisk-ls.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-echo "Enter your name:"
+echo "Enter your name:" | lolcat
 read name
 #Copy the following code to motd.sh
 echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶"
@@ -14,4 +14,4 @@ rm /data/data/com.termux/files/usr/etc/motd.sh
 #Move the created motd.sh file to profile.d directory
 mv motd.sh /data/data/com.termux/files/usr/etc/profile.d
 clear
-echo "Please restart Termux, to confirm changes. Enjoy😍"
+echo "Please restart Termux, to confirm changes. Enjoy😍" | lolcat
